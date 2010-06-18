406a70dd0115780e6163bd24370459af80f00147
Ask Solem <askh@opera.com>
2010-06-18 16:19:16 +0200

    Cleanup pika.channel

--- pika/channel.py
+++ pika/channel.py
@@ -46,13 +46,13 @@
 #
 # ***** END LICENSE BLOCK *****
 
-import pika.spec as spec
-import pika.codec as codec
-import pika.event as event
-from pika.exceptions import *
+from pika import spec
+from pika import codec
+from pika import event
+from pika import exception
 
-class ChannelHandler:
-    def __init__(self, connection, channel_number = None):
+class ChannelHandler(object):
+    def __init__(self, connection, channel_number=None):
         self.connection = connection
         self.frame_handler = self._handle_method
         self.channel_close = None
@@ -71,17 +71,18 @@ class ChannelHandler:
 
     def _async_channel_close(self, method_frame, header_frame, body):
         self._set_channel_close(method_frame.method)
-        self.connection.send_method(self.channel_number, spec.Channel.CloseOk())
+        self.connection.send_method(self.channel_number,
+                                    spec.Channel.CloseOk())
 
     def _async_channel_flow(self, method_frame, header_frame, body):
         self.flow_active = method_frame.method.active
         self.flow_active_change_event.fire(self, self.flow_active)
         self.connection.send_method(self.channel_number,
-                                    spec.Channel.FlowOk(active = self.flow_active))
+                                spec.Channel.FlowOk(active=self.flow_active))
 
     def _ensure(self):
         if self.channel_close:
-            raise ChannelClosed(self.channel_close)
+            raise exceptions.ChannelClosed(self.channel_close)
         return self
 
     def _set_channel_close(self, c):
@@ -90,11 +91,11 @@ class ChannelHandler:
             self.connection.reset_channel(self.channel_number)
             self.channel_state_change_event.fire(self, False)
 
-    def addStateChangeHandler(self, handler, key = None):
+    def addStateChangeHandler(self, handler, key=None):
         self.channel_state_change_event.addHandler(handler, key)
         handler(self, not self.channel_close)
 
-    def addFlowChangeHandler(self, handler, key = None):
+    def addFlowChangeHandler(self, handler, key=None):
         self.flow_active_change_event.addHandler(handler, key)
         handler(self, self.flow_active)
 
@@ -104,7 +105,8 @@ class ChannelHandler:
             return
 
         if self.reply_map is not None:
-            raise RecursiveOperationDetected([p.NAME for p in acceptable_replies])
+            raise exceptions.RecursiveOperationDetected([p.NAME
+                    for p in acceptable_replies])
 
         reply = [None]
         def set_reply(r):
@@ -117,37 +119,40 @@ class ChannelHandler:
         while True:
             self._ensure()
             self.connection.drain_events()
-            if reply[0]: return reply[0]
+            if reply[0]:
+                return reply[0]
 
     def _handle_async(self, method_frame, header_frame, body):
         method = method_frame.method
-        methodClass = method.__class__
+        method_cls = method.__class__
 
-        if self.reply_map is not None and methodClass in self.reply_map:
+        if self.reply_map is not None and method_cls in self.reply_map:
             if header_frame is not None:
                 method._set_content(header_frame.properties, body)
-            handler = self.reply_map[methodClass]
+            handler = self.reply_map[method_cls]
             self.reply_map = None
             handler(method)
-        elif methodClass in self.async_map:
-            self.async_map[methodClass](method_frame, header_frame, body)
+        elif method_cls in self.async_map:
+            self.async_map[method_cls](method_frame, header_frame, body)
         else:
             self.connection.close(spec.NOT_IMPLEMENTED,
-                                  'Pika: method not implemented: ' + methodClass.NAME)
+                    "Pika: method not implemented: %s" % (method_cls.NAME, ))
 
     def _handle_method(self, frame):
         if not isinstance(frame, codec.FrameMethod):
-            raise UnexpectedFrameError(frame)
+            raise exceptions.UnexpectedFrameError(frame)
         if spec.has_content(frame.method.INDEX):
             self.frame_handler = self._make_header_handler(frame)
         else:
             self._handle_async(frame, None, None)
 
     def _make_header_handler(self, method_frame):
+
         def handler(header_frame):
             if not isinstance(header_frame, codec.FrameHeader):
-                raise UnexpectedFrameError(header_frame)
+                raise exceptions.UnexpectedFrameError(header_frame)
             self._install_body_handler(method_frame, header_frame)
+
         return handler
 
     def _install_body_handler(self, method_frame, header_frame):
@@ -156,19 +161,21 @@ class ChannelHandler:
 
         def handler(body_frame):
             if not isinstance(body_frame, codec.FrameBody):
-                raise UnexpectedFrameError(body_frame)
+                raise exceptions.UnexpectedFrameError(body_frame)
             fragment = body_frame.fragment
-            seen_so_far[0] = seen_so_far[0] + len(fragment)
+            seen_so_far[0] += len(fragment)
             body_fragments.append(fragment)
             if seen_so_far[0] == header_frame.body_size:
                 finish()
             elif seen_so_far[0] > header_frame.body_size:
-                raise BodyTooLongError()
+                raise exceptions.BodyTooLongError()
             else:
                 pass
+
         def finish():
             self.frame_handler = self._handle_method
-            self._handle_async(method_frame, header_frame, ''.join(body_fragments))
+            self._handle_async(method_frame, header_frame,
+                               ''.join(body_fragments))
 
         if header_frame.body_size == 0:
             finish()
@@ -177,35 +184,43 @@ class ChannelHandler:
 
     def _rpc(self, method, acceptable_replies):
         self._ensure()
-        return self.connection._rpc(self.channel_number, method, acceptable_replies)
+        return self.connection._rpc(self.channel_number, method,
+                                    acceptable_replies)
 
     def content_transmission_forbidden(self):
         return not self.flow_active
 
 class Channel(spec.DriverMixin):
+
     def __init__(self, handler):
         self.handler = handler
         self.callbacks = {}
         self.pending = {}
         self.next_consumer_tag = 0
 
-        handler.async_map[spec.Channel.Close] = handler._async_channel_close
-        handler.async_map[spec.Channel.Flow] = handler._async_channel_flow
-
-        handler.async_map[spec.Basic.Deliver] = self._async_basic_deliver
-        handler.async_map[spec.Basic.Return] = self._async_basic_return
+        handler.async_map.update({
+            spec.Channel.Close: handler._async_channel_close,
+            spec.Channel.Flow: handler._async_channel_flow,
+            spec.Basic.Deliver: handler._async_basic_deliver,
+            spec.Basic.Return: handler._async_basic_return,
+        })
 
         self.handler._rpc(spec.Channel.Open(), [spec.Channel.OpenOk])
 
-    def addStateChangeHandler(self, handler, key = None):
+    def addStateChangeHandler(self, handler, key=None):
+        # TODO state_change_handlers.append(handler, key) ?
         self.handler.addStateChangeHandler(handler, key)
 
-    def addFlowChangeHandler(self, handler, key = None):
+    def addFlowChangeHandler(self, handler, key=None):
         self.handler.addFlowChangeHandler(handler, key)
 
     def _async_basic_deliver(self, method_frame, header_frame, body):
-        """Cope with reentrancy. If a particular consumer is still active when another
-        delivery appears for it, queue the deliveries up until it finally exits."""
+        """Cope with reentrancy.
+
+        If a particular consumer is still active when another delivery
+        appears for it, queue the deliveries up until it finally exits.
+
+        """
         consumer_tag = method_frame.method.consumer_tag
         if consumer_tag not in self.pending:
             q = []
@@ -215,60 +230,64 @@ class Channel(spec.DriverMixin):
             while q:
                 (m, p, b) = q.pop(0)
                 consumer(self, m, p, b)
-            del self.pending[consumer_tag]
+            del(self.pending[consumer_tag])
         else:
-            self.pending[consumer_tag].append((method_frame.method, header_frame.properties, body))
+            self.pending[consumer_tag].append((method_frame.method,
+                                               header_frame.properties, body))
 
     def _async_basic_return(self, method_frame, header_frame, body):
         raise NotImplementedError("Basic.Return")
 
-    def close(self, code = 0, text = 'Normal close'):
+    def close(self, code=0, text='Normal close'):
         try:
-            c = spec.Channel.Close(reply_code = code,
-                                   reply_text = text,
-                                   class_id = 0,
-                                   method_id = 0)
+            c = spec.Channel.Close(reply_code=code,
+                                   reply_text=text,
+                                   class_id=0,
+                                   method_id=0)
             self.handler._rpc(c, [spec.Channel.CloseOk])
-        except ChannelClosed:
+        except exceptions.ChannelClosed:
             pass
         self.handler._set_channel_close(c)
         return self.handler.channel_close
 
-    def basic_publish(self, exchange, routing_key, body, properties = None, mandatory = False, immediate = False, block_on_flow_control = False):
+    def basic_publish(self, exchange, routing_key, body, properties=None,
+            mandatory=False, immediate=False, block_on_flow_control=False):
         if self.handler.content_transmission_forbidden():
             if block_on_flow_control:
                 while self.handler.content_transmission_forbidden():
                     self.handler.connection.drain_events()
             else:
-                raise ContentTransmissionForbidden(self)
-        properties = properties or spec.BasicProperties()
+                raise exceptions.ContentTransmissionForbidden(self)
+        if properties is None:
+            properties = spec.BasicProperties()
+        method = spec.Basic.Publish(exchange=exchange,
+                                    routing_key=routing_key,
+                                    mandatory=mandatory,
+                                    immediate=immediate)
         self.handler.connection.send_method(self.handler.channel_number,
-                                            spec.Basic.Publish(exchange = exchange,
-                                                               routing_key = routing_key,
-                                                               mandatory = mandatory,
-                                                               immediate = immediate),
-                                            (properties, body))
+                                            method, (properties, body))
 
-    def basic_consume(self, consumer, queue = '', no_ack = False, exclusive = False, consumer_tag = None):
+    def basic_consume(self, consumer, queue='', no_ack=False, exclusive=False,
+            consumer_tag=None):
         tag = consumer_tag
         if not tag:
             tag = 'ctag' + str(self.next_consumer_tag)
             self.next_consumer_tag += 1
 
         if tag in self.callbacks:
-            raise DuplicateConsumerTag(tag)
+            raise exceptions.DuplicateConsumerTag(tag)
 
         self.callbacks[tag] = consumer
-        return self.handler._rpc(spec.Basic.Consume(queue = queue,
-                                                    consumer_tag = tag,
-                                                    no_ack = no_ack,
-                                                    exclusive = exclusive),
+        return self.handler._rpc(spec.Basic.Consume(queue=queue,
+                                                    consumer_tag=tag,
+                                                    no_ack=no_ack,
+                                                    exclusive=exclusive),
                                  [spec.Basic.ConsumeOk]).consumer_tag
 
     def basic_cancel(self, consumer_tag):
         if not consumer_tag in self.callbacks:
-            raise UnknownConsumerTag(consumer_tag)
+            raise exceptions.UnknownConsumerTag(consumer_tag)
 
-        self.handler._rpc(spec.Basic.Cancel(consumer_tag = consumer_tag),
+        self.handler._rpc(spec.Basic.Cancel(consumer_tag=consumer_tag),
                           [spec.Basic.CancelOk])
-        del self.callbacks[consumer_tag]
+        del(self.callbacks[consumer_tag])
